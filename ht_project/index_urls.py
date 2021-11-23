7bd5a61868584e3315bb06f3b893f57cedc3d712
Andrea Lampugnani <andrea.lampugnani02@gmail.com>
2021-11-23 09:22:41 +0000

    scrape every page

--- ht_project/index_urls.py
+++ ht_project/index_urls.py
@@ -5,23 +5,29 @@ import csv
 import pandas as pd
 
 def take_url():
-    lista_finale_url = list()
+    all_url_list = list()
     for i in range(1, 69):
         response = requests.get(
             "http://antislavery.ac.uk/solr-search?facet=collection%3A%22VOICES%3A+Narratives+by+Survivors+of+Modern+Slavery%22&page={number}"
             .format(number=i))
+
+        # I take the html response
         html = response.text
         soup = BeautifulSoup(html,features="html.parser")
-        img = soup.find_all("a")
-        lista = [i.get("href") for i in img]
+        a = soup.find_all("a")
+
+        #I get all the url inside my list a
+        lista = [i.get("href") for i in a]
         for i in lista:
             if type(i) == str:
                 if re.findall(".*items.*", i):
-                    lista_finale_url.append(i)
+                    all_url_list.append(i)
 
-    final_list = ["http://antislavery.ac.uk" + i for i in lista_finale_url]
+    # I add at the beginning of every url inside all_url_list this path: http://antislavery.ac.uk
+    final_list = ["http://antislavery.ac.uk" + i for i in all_url_list]
     return final_list
 
 list_url=take_url()
 
+#creating a DataFrame with the url list
 df = pd.DataFrame(list_url)
---
+++ ht_project/scrape_page.py
@@ -0,0 +1,30 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+import pandas as pd
+
+path = "raw_data/index_url.csv"
+df=pd.read_csv(path)
+
+
+def scrape():
+    get_features=list()
+    for i in range(2):
+        link = df.loc[i].url
+        response = requests.get(link)
+        html = response.text
+        soup = BeautifulSoup(html, features="html.parser")
+        p = soup.find("div", {"class": "text-item-body no-select"}).find_all("p")
+        list_principal_text = ",".join([i.text for i in p if i.text != "\xa0"])
+        take = soup.find("dd", {"class": "item-meta__value"}).text
+        countries = re.findall("\n..(.*)", take)[0]
+        departure = re.findall("(.*).slavery location", countries)[0].strip()
+        arrival = re.findall("slavery location.*?(\w.*) .trafficked from",
+                        countries)[0]
+        typee = soup.find_all("a", {"class": "no-underline"})
+        theme = typee[2].text
+        return [take, departure, arrival, theme]
+
+
+scrape_page = scrape()
+print(scrape_page[0])
